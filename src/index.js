bb748401e5598f7d8e48eff25d26584a52d93d19
AndrezaAmorim <amrimandreza@gmail.com>
2019-12-01 17:03:46 -0300

    Criação da função destroyMessage e alteração de css

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-document.querySelector('.btn-cifrar').addEventListener("click", messageEncrypted); // A ação do click deve ser chamado no index.js
-document.querySelector('.btn-decifrar').addEventListener("click", messageDeciphered);
-
-function messageEncrypted() {
+const messageEncrypted = () => {
   event.preventDefault();
-  let text = document.querySelector('.message').value;
-  let key = Number (document.querySelector('.key').value);
-  let result = document.querySelector('.result');
+  let text = document.querySelector(".message").value;
+  let key = Number (document.querySelector(".key").value);
+  let result = document.querySelector(".result");
   let encrypted = window.cipher.encode(key, text);
   result.innerHTML=`<p class="highlighter"> Mensagem codificada: ${encrypted}<p>`; // highlighter imprime com marca texto.
-}
+};
  
-function messageDeciphered() {
+const messageDeciphered = () => {
   event.preventDefault();
-  let text = document.querySelector('.message').value;
-  let key = document.querySelector('.key').value;
-  let result = document.querySelector('.result');
+  let text = document.querySelector(".message").value;
+  let key = document.querySelector(".key").value;
+  let result = document.querySelector(".result");
   let deciphered = window.cipher.decode(key, text);
   result.innerHTML=`<p class="highlighter"> Mensagem decodificada: ${deciphered}<p>`;
-}
-/*Cria a caixa (input) no html, usuario ira colocar o texto e ao clicar no botao esse texto sera 
- chamado no index.js e colocado em uma variavel. Ainda no index.js voce chama
- a função criada no cipher.js para que ele realize a criptografia e retorne o valor para o index.js. */
+};
+
+const destroyMessage = () => {
+  location.reload();
+};
+
+document.querySelector(".btn-destruir").addEventListener("click", destroyMessage); 
+document.querySelector(".btn-cifrar").addEventListener("click", messageEncrypted); 
+document.querySelector(".btn-decifrar").addEventListener("click", messageDeciphered);
