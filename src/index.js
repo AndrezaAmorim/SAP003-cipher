c2be0ac225d1843d0b81e168b5fb6d18aa63f243
AndrezaAmorim <amrimandreza@gmail.com>
2019-12-08 19:36:43 -0300

    Atualização do ReadMe, CSS e HTML

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const messageEncrypted = () => {
   let key = Number (document.querySelector(".key").value);
   let result = document.querySelector(".result");
   let encrypted = window.cipher.encode(key, text);
-  result.innerHTML=`<p class="highlighter"> Mensagem codificada: ${encrypted}<p>`; // highlighter imprime com marca texto.
+  result.innerHTML=`<p> Mensagem codificada: ${encrypted}<p>`;
 };
  
 const messageDeciphered = () => {
@@ -13,13 +13,13 @@ const messageDeciphered = () => {
   let key = document.querySelector(".key").value;
   let result = document.querySelector(".result");
   let deciphered = window.cipher.decode(key, text);
-  result.innerHTML=`<p class="highlighter"> Mensagem decodificada: ${deciphered}<p>`;
+  result.innerHTML=`<p> Mensagem decodificada: ${deciphered}<p>`;
 };
 
-const destroyMessage = () => {
-  location.reload();
+const eraseMessage = () => {
+  Location.reload();
 };
 
-document.querySelector(".btn-destruir").addEventListener("click", destroyMessage); 
+document.querySelector(".btn-apagar").addEventListener("click", eraseMessage); 
 document.querySelector(".btn-cifrar").addEventListener("click", messageEncrypted); 
 document.querySelector(".btn-decifrar").addEventListener("click", messageDeciphered);
