d5b503ba20364f0b84dd97f3f0a7147e02ffa730
AndrezaAmorim <amrimandreza@gmail.com>
2019-12-28 15:15:50 -0300

    Inclusão de For in no codigo

--- src/cipher.js
+++ src/cipher.js
@@ -5,16 +5,16 @@ window.cipher = {
 
 function encode(offset, text) {
   let result=[];
-
-  for (let i=0; i<text.length; i++) {
-    if (text[i].charCodeAt()>=65 && text[i].charCodeAt()<=90) {
-      result += String.fromCharCode(((text[i].charCodeAt()-65+offset)%26)+65);
+  
+  for (let prop in text) {
+    if (text[prop].charCodeAt()>=65 && text[prop].charCodeAt()<=90) {
+      result += String.fromCharCode(((text[prop].charCodeAt()-65+offset)%26)+65);
     }
-    else if (text[i].charCodeAt()>=97 && text[i].charCodeAt()<=122) {
-      result += String.fromCharCode(((text[i].charCodeAt()-97+offset)%26)+97);
+    else if (text[prop].charCodeAt()>=97 && text[prop].charCodeAt()<=122) {
+      result += String.fromCharCode(((text[prop].charCodeAt()-97+offset)%26)+97);
     }
     else {
-      result += text[i];
+      result += text[prop];
     }
   }
   return result;
@@ -22,15 +22,16 @@ function encode(offset, text) {
 
 function decode(offset, text) {
   let result=[];
-  for (let i=0; i<text.length; i++) {
-    if (text[i].charCodeAt()>=65 && text[i].charCodeAt()<=90) {
-      result += String.fromCharCode(((text[i].charCodeAt()-90-offset)%26)+90);
+   
+  for (let prop in text) {
+    if (text[prop].charCodeAt()>=65 && text[prop].charCodeAt()<=90) {
+      result += String.fromCharCode(((text[prop].charCodeAt()-90-offset)%26)+90);
     }
-    else if (text[i].charCodeAt()>=97 && text[i].charCodeAt()<=122) {
-      result += String.fromCharCode(((text[i].charCodeAt()-122-offset)%26)+122);
+    else if (text[prop].charCodeAt()>=97 && text[prop].charCodeAt()<=122) {
+      result += String.fromCharCode(((text[prop].charCodeAt()-122-offset)%26)+122);
     }
     else {
-      result += text[i];
+      result += text[prop];
     }
   }
   return result;
